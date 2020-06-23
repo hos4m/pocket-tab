2c90b52c76962db0c858a47140e26e410ef97078
Hossam Mourad <hossam.mourad@fullscript.com>
2020-06-23 11:11:49 -0400

    util to authorize and get data from pocket API

--- index.mjs
+++ index.mjs
@@ -1,5 +1,12 @@
 import "https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.4.1/dist/alpine.min.js";
 import includeHTML from "./utils/includeHTML.mjs";
+import getPocketData from "./utils/getPocketData.mjs";
 
 // Initializers
-includeHTML();
+(async function () {
+  includeHTML();
+  const pocketData = await getPocketData();
+  console.log("----------------------------");
+  console.log(pocketData);
+  console.log("----------------------------");
+})();
---
+++ utils/getPocketData.mjs
@@ -0,0 +1,68 @@
+import pocketConsumerKey from "../configs/pocketConsumerKey.mjs";
+
+export default async function getPocketData() {
+  const base = "https://getpocket.com/v3";
+  const consumer_key = pocketConsumerKey;
+  const redirect_uri = window.origin;
+  const requestTokenLS = localStorage.getItem("request_token");
+  const accessTokenLS = localStorage.getItem("access_token");
+
+  // Step 1: get a request token
+  if (!requestTokenLS) {
+    try {
+      const response = await fetch(
+        `${base}/oauth/request?consumer_key=${consumer_key}&redirect_uri=${redirect_uri}`,
+        {
+          method: "post",
+        }
+      );
+      if (response.ok) {
+        const data = await response.text();
+        const token = data.split("=")[1];
+        localStorage.setItem("request_token", token);
+        window.location = `https://getpocket.com/auth/authorize?request_token=${token}&redirect_uri=${redirect_uri}`;
+      }
+    } catch (_) {}
+  }
+
+  // Step 2: get an access token
+  if (requestTokenLS && !accessTokenLS) {
+    try {
+      const response = await fetch(
+        `${base}/oauth/authorize?consumer_key=${consumer_key}&code=${requestTokenLS}`,
+        {
+          method: "post",
+        }
+      );
+      if (response.ok) {
+        const data = await response.text();
+        const accessToken = data.split("&")[0].split("=")[1];
+        const username = data.split("&")[1].split("=")[1];
+        localStorage.setItem("access_token", accessToken);
+        localStorage.setItem("username", username);
+        window.location.reload();
+      } else {
+        window.location = `https://getpocket.com/auth/authorize?request_token=${requestTokenLS}&redirect_uri=${redirect_uri}`;
+      }
+    } catch (_) {}
+  }
+
+  // Step 3: get the final data
+  if (accessTokenLS) {
+    try {
+      const response = await fetch(
+        `${base}/get?consumer_key=${consumer_key}&access_token=${accessTokenLS}`,
+        {
+          method: "post",
+        }
+      );
+      if (response.ok) {
+        const data = await response.json();
+        return data;
+      } else {
+        localStorage.clear();
+        window.location.reload();
+      }
+    } catch (_) {}
+  }
+}
