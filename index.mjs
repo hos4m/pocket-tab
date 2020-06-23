f9279cfb8e209bb8b22dbedb8c59d8664f872c87
Hossam Mourad <hossam.mourad@fullscript.com>
2020-06-23 06:58:28 -0400

    introduce a way to render partials in HTML using pure JS

--- index.mjs
+++ index.mjs
@@ -1,1 +1,5 @@
 import "https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.4.1/dist/alpine.min.js";
+import includeHTML from "./utils/includeHTML.mjs";
+
+// Initializers
+includeHTML();
---
+++ utils/includeHTML.mjs
@@ -0,0 +1,33 @@
+// source: https://www.w3schools.com/howto/howto_html_include.asp
+
+export default function includeHTML() {
+  var z, i, element, file, xhttp;
+  /* Loop through a collection of all HTML elements: */
+  z = document.getElementsByTagName("*");
+  for (i = 0; i < z.length; i++) {
+    element = z[i];
+    /*search for elements with a certain attribute:*/
+    file = element.getAttribute("include-html");
+    if (file) {
+      /* Make an HTTP request using the attribute value as the file name: */
+      xhttp = new XMLHttpRequest();
+      xhttp.onreadystatechange = function () {
+        if (this.readyState == 4) {
+          if (this.status == 200) {
+            element.innerHTML = this.responseText;
+          }
+          if (this.status == 404) {
+            element.innerHTML = "Page not found.";
+          }
+          /* Remove the attribute, and call this function once more: */
+          element.removeAttribute("include-html");
+          includeHTML();
+        }
+      };
+      xhttp.open("GET", file, true);
+      xhttp.send();
+      /* Exit the function: */
+      return;
+    }
+  }
+}
