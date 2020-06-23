a0b064773bc4c6586ea6394a889ae43b8a3431fa
Hossam Mourad <hossam.mourad@fullscript.com>
2020-06-23 11:16:10 -0400

    file rename

--- utils/getPocketData.mjs
+++ utils/getPocketData.mjs
@@ -1,4 +1,4 @@
-import pocketConsumerKey from "../configs/pocketConsumerKey.mjs";
+import { pocketConsumerKey } from "../configs/keys.mjs";
 
 export default async function getPocketData() {
   const base = "https://getpocket.com/v3";
